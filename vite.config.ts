3017cde3c66340bebbcb1e8c8b78a5cc8fba76a3
Samiyo <samiyokim@gmail.com>
2025-02-24 18:55:12 -0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/",
+  base: "/fetch-demo/",
   plugins: [react(), tailwindcss(),],
   build: {
     outDir: 'dist'
